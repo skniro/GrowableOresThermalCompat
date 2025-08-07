290a4df613542b73f924f3f17dc841b24957bb53
skniro <46392259+skniro@users.noreply.github.com>
2025-08-07 21:32:39 +0800

    Add support.

---
+++ Forge/src/main/java/com/skniro/growable_ores_thermal_compat/GrowableOresThermalCompat.java
@@ -0,0 +1,21 @@
+package com.skniro.growable_ores_thermal_compat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(GrowableOresThermalCompat.MODID)
+public class GrowableOresThermalCompat {
+    public static final String MODID = "growable_ores_thermal_compat";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public GrowableOresThermalCompat() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
